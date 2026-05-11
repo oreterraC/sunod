b2341fca0a805da701afed45d6784c170eaa98cf
oreterraC <diegolanseiros@gmail.com>
2026-05-11 12:23:35 +0200

    Install TailwindCSS

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-size-900 text-white flex items-center justify-center">
+      <h1 className="text-5xl front-bold text-cyan-400">
+        Tailwind funcionando
+      </h1>
+    </div>
+  );
+}
+
+export default App;
