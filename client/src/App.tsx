8d89938a7605958effefad4a99ebb93101a7647d
oreterraC <diegolanseiros@gmail.com>
2026-05-14 12:46:15 +0200

    Build the color mode switch

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,21 @@
+import { useState } from "react";
 import "./App.css";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <>
+    <div className={darkMode ? "dark" : ""}>
       <div className="grid grid-rows-[auto_1fr] grid-cols-1 lg:grid-cols-[200px_1fr] min-h-screen">
-        <NavBar />
+        <NavBar
+          darkMode={darkMode}
+          onChange={() => setDarkMode((darkMode) => !darkMode)}
+        />
         <aside className="hidden lg:block bg-gray-800 text-white p-4"></aside>
-        <main className="bg-gray-300 p-4"></main>
+        <main className="bg-gray-600 p-4"></main>
       </div>
-    </>
+    </div>
   );
 }
 
--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,19 +1,19 @@
-import { useState } from "react";
 import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import logo from "../assets/logo.png";
 
-const NavBar = () => {
-  const [darkMode, setDarkMode] = useState(true);
+interface Properties {
+  darkMode: boolean;
+  onChange: () => void;
+}
+
+const NavBar = ({ darkMode, onChange }: Properties) => {
+  const ThemeIcon = darkMode ? SunIcon : MoonIcon;
 
   return (
     <nav className="h-18 bg-gray-900 flex justify-between text-white lg:col-span-2 p-4">
       <img src={logo} className="h-10 w-auto" />
-      <button onClick={() => setDarkMode(!darkMode)}>
-        {darkMode ? (
-          <SunIcon className="h-6 w-auto pr-3" />
-        ) : (
-          <MoonIcon className="h-6 w-auto pr-3" />
-        )}
+      <button onClick={onChange}>
+        <ThemeIcon className="h-6 w-auto pr-3" />
       </button>
     </nav>
   );
