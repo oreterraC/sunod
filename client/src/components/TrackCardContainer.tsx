509bdd4dee49b4028655db3be6ac1a3f4b68dce5
oreterraC <diegolanseiros@gmail.com>
2026-05-20 12:30:26 +0200

    Display songs

--- client/src/components/TrackCardContainer.tsx
+++ client/src/components/TrackCardContainer.tsx
@@ -1,5 +1,18 @@
-const TrackCardContainer = () => {
-  return <div className="bg-gray-400 rounded-sm"></div>;
+interface Song {
+  id: number;
+  title: string;
+}
+
+interface Properties {
+  song: Song;
+}
+
+const TrackCardContainer = ({ song }: Properties) => {
+  return (
+    <div className="bg-gray-400 rounded-sm">
+      {song.id} {song.title}
+    </div>
+  );
 };
 
 export default TrackCardContainer;
--- client/src/components/TrackGrid.tsx
+++ client/src/components/TrackGrid.tsx
@@ -1,12 +1,31 @@
+import { useEffect, useState } from "react";
 import TrackCardContainer from "./TrackCardContainer";
 
+interface Song {
+  id: number;
+  title: string;
+}
+
 const TrackGrid = () => {
-  const list = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const [songs, setSongs] = useState<Song[]>([]);
+
+  useEffect(() => {
+    const fetchSongs = async () => {
+      const response = await fetch(
+        `http://localhost:5000/songs/search?q=eminem`,
+      );
+
+      const data = await response.json();
+      setSongs(data);
+    };
+
+    fetchSongs();
+  }, []);
 
   return (
     <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 bg-gray-600 p-4">
-      {list.map((item) => (
-        <TrackCardContainer />
+      {songs?.map((song) => (
+        <TrackCardContainer song={song} />
       ))}
     </div>
   );
